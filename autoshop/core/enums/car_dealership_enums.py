ad8e1a0e92e7e4505a41b7052c906ea35c448b86
miller001155 <miller00115@gmail.com>
2022-12-10 14:09:36 +0300

    newhardskills

---
+++ autoshop/core/enums/car_dealership_enums.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Currency(models.TextChoices):
+    EUR = 'EUR', 'EUR'
+    USD = 'USD', 'USD'
+    RUB = 'RUB', 'RUB'
+
+
+class StatusOfCar(models.TextChoices):
+    Available = 'Available', 'Available'
+    Out_of_stock = 'Out_of_stock', 'Out_of_stock'
